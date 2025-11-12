c8a041ee4a554028b1cbc702541f4469d86fe001
FoyerD <amitfoye@post.bgu.ac.il>
2025-11-12 13:43:50 +0200

    changed from float to int

--- color_blocks/color_blocks_state.py
+++ color_blocks/color_blocks_state.py
@@ -1,28 +1,28 @@
 import numpy as np
 from numpy.typing import NDArray
 
-global_goal_state : NDArray[np.float32] = np.array([])
+global_goal_state : NDArray[np.int32] = np.array([])
 
 def init_goal_for_search(goal_blocks: str) -> None:
     global global_goal_state
     global_goal_state = np.array(goal_blocks.split(','))
 
 
-def parse_state_to_ndarray(blocks_str: str) -> NDArray[np.float32]:
+def parse_state_to_ndarray(blocks_str: str) -> NDArray[np.int32]:
     return np.array([])
     
 
 class color_blocks_state:
-    def __init__(self, blocks_str: str, g: np.float32=np.float32(0), h: np.float32=np.float32(0)):
-        self.state_array : NDArray[np.float32] = parse_state_to_ndarray(blocks_str)
-        self.g : np.float32 = g
-        self.h : np.float32 = h
-        self.f : np.float32 = g + h
+    def __init__(self, blocks_str: str, g: np.int32=np.int32(0), h: np.int32=np.int32(0)):
+        self.state_array : NDArray[np.int32] = parse_state_to_ndarray(blocks_str)
+        self.g : np.int32 = g
+        self.h : np.int32 = h
+        self.f : np.int32 = g + h
         
 
     @staticmethod
     def is_goal_state(_color_blocks_state):
-        pass
+        return 
 
     def get_neighbors(self):
         pass
@@ -38,6 +38,8 @@ class color_blocks_state:
     def get_state_str(self):
         pass
 
-
+    @override
+    def __eq__(self, other):
+        return self.state_array == other.state_array
 
     #you can add helper functions
