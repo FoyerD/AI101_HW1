93018d27c83e750a0466f60e3aa9cca7f1511ee2
ItayTk <itaytk@post.bgu.ac.il>
2025-11-12 13:23:30 +0200

    finished init_goal_for_heuristics

--- color_blocks/heuristics.py
+++ color_blocks/heuristics.py
@@ -1,11 +1,12 @@
 from color_blocks_state import color_blocks_state
+import numpy as np
 # you can add helper functions and params
 
 global_goal_blocks = ''
 
 def init_goal_for_heuristics(goal_blocks: str) -> None:
     global global_goal_blocks
-    global_goal_blocks = goal_blocks
+    global_goal_blocks = np.array(list(map(int, goal_blocks.split(","))), dtype=int)
 
 def base_heuristic(_color_blocks_state: color_blocks_state):
     
