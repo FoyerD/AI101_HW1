54029a2c35fd6b88f7edc29bdc0cf70a5dc2ba85
ItayTk <itaytk@post.bgu.ac.il>
2025-11-12 13:51:15 +0200

    started base_heuristic

--- color_blocks/heuristics.py
+++ color_blocks/heuristics.py
@@ -6,10 +6,13 @@ global_goal_blocks = ''
 
 def init_goal_for_heuristics(goal_blocks: str) -> None:
     global global_goal_blocks
-    global_goal_blocks = np.array(list(map(int, goal_blocks.split(","))), dtype=int)
+    global_goal_blocks = np.array(list(map(np.int32, goal_blocks.split(","))), dtype=np.int32)
 
 def base_heuristic(_color_blocks_state: color_blocks_state):
-    
+    base_sum = 0
+    for i in range(len(color_blocks_state.state_array)-1):
+        pass
+    return base_sum
 
 def advanced_heuristic(_color_blocks_state):
     return 0
