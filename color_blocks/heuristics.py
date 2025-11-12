799c747e3fbb689be181174d9f3a640694045b4d
FoyerD <amitfoye@post.bgu.ac.il>
2025-11-12 12:56:52 +0200

    initial commit

---
+++ color_blocks/heuristics.py
@@ -0,0 +1,15 @@
+from color_blocks_state import color_blocks_state
+# you can add helper functions and params
+
+global_goal_blocks = ''
+
+def init_goal_for_heuristics(goal_blocks: str) -> None:
+    global global_goal_blocks
+    global_goal_blocks = goal_blocks
+
+def base_heuristic(_color_blocks_state: color_blocks_state):
+    return 0
+
+def advanced_heuristic(_color_blocks_state):
+    return 0
+
